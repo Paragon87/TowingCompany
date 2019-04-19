819f88d4540946ec2cf1bbabc457fb8568d5c8f4
Paragon87 <goff3110@gmail.com>
2019-04-19 06:38:13 -0500

    Update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-    $("#fbButton").on("click", function () {
+    $(".fbButton").on("click", function () {
         window.open("https://www.facebook.com/Mercado-and-Sons-Towing-673109756480032", "_blank");
     });
 
