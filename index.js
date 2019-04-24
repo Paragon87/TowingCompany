c0ace39309f67b98b42e4e01fff6069ad8ee1990
Paragon87 <goff3110@gmail.com>
2019-04-23 21:05:13 -0500

    Function Update

--- index.js
+++ index.js
@@ -1,69 +1,67 @@
 $(document).ready(function() {
-
     
-});
-//Open business Facebook page function
-$("#fbButton").on("click", function () {
-    window.open("https://www.facebook.com/Mercado-and-Sons-Towing-673109756480032", "_blank");
-});
+    //Open business Facebook page function
+    $("#fbButton").on("click", function () {
+        window.open("https://www.facebook.com/Mercado-and-Sons-Towing-673109756480032", "_blank");
+    });
 
-//Pointer hover color change functions
-$("#fbButton").hover(function() {
-    $(this).css("color", "grey");
-},
-function() {
-    $(this).css("color", "white");
-});
+    //Pointer hover color change functions
+    $("#fbButton").hover(function() {
+        $(this).css("color", "grey");
+    },
+    function() {
+        $(this).css("color", "white");
+    });
 
-$("#emailButton").hover(function() {
-    $(this).css("color", "grey");
-},
-function() {
-    $(this).css("color", "white");
-});
+    $("#emailButton").hover(function() {
+        $(this).css("color", "grey");
+    },
+    function() {
+        $(this).css("color", "white");
+    });
 
-$("#callButton").hover(function() {
-    $(this).css("color", "grey");
-},
-function() {
-    $(this).css("color", "white");
-});
+    $("#callButton").hover(function() {
+        $(this).css("color", "grey");
+    },
+    function() {
+        $(this).css("color", "white");
+    });
 
-// Validating Empty Field
-function check_empty() {
-    if (document.getElementById('name').value == "" || document.getElementById('email').value == "" || document.getElementById('msg').value == "") {
-        alert("Fill All Fields !");
-    } else {
-        window.open(mail+recipient+at+dotcom+sender+senderEmail+message);
-        alert("Form Submitted Successfully...");
-    }
-}
+    //Send email
+    var sender = "?sender=" + document.getElementById("name").value;
+    var senderEmail = "senderEmail=" + document.getElementById("email").value;
+    var message = "body=" + document.getElementById("msg").value;
+    var recipient="goff3110";
+    var at = String.fromCharCode(64);
+    var dotcom="gmail.com";
+    var mail="mailto:";
 
-//Function To Display Popup
-function div_show() {
+    // Validating Empty Field
+    function check_empty() {
+        if (document.getElementById('name').value == "" || document.getElementById('email').value == "" || document.getElementById('msg').value == "") {
+            alert("Fill All Fields !");
+        } else {
+            window.open(mail+recipient+at+dotcom+sender+senderEmail+message);
+            alert("Form Submitted Successfully...");
+        }
+    };
+
+    //Function To Display Popup
+    function div_show() {
     document.getElementById('emailPopup').style.display = "block";
-};
+    };
 
-//Function to Hide Popup
-function div_hide() {
-    document.getElementById('emailPopup').style.display = "none";
-};
+    //Function to Hide Popup
+    function div_hide() {
+        document.getElementById('emailPopup').style.display = "none";
+    };
 
-//Open email popup button
-$("#emailButton").on("click", div_show);
+    //Open email popup button
+    $("#emailButton").on("click", div_show);
 
-//Close email popup button
-$("#close").on("click", div_hide);
+    //Close email popup button
+    $("#close").on("click", div_hide);
 
-//Submit form button
-$("submit").on("click", check_empty);
-
-//Send email
-var sender = "?sender=" + document.getElementById("name");
-var senderEmail = "senderEmail=" + document.getElementById("email");
-var message = "body=" + document.getElementById("msg");
-var recipient="goff3110";
-var at = String.fromCharCode(64);
-var dotcom="gmail.com";
-var mail="mailto:";
- 
+    //Submit form button
+    $("submit").on("click", check_empty);
+});
