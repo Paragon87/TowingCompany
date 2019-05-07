19b78f493a7edb841d7d9fa980b9668b9d68581c
Geoffrey G <goff3110@gmail.com>
2019-05-07 06:22:36 -0500

    Update index.js

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ $(document).ready(function() {
             var mail="mailto:";
 
             var mailWindow = window.open(mail + recipient + at + dotcom + subject + body);
-            setTimeout(function(){mailWindow.close()}, 2000);
+            setTimeout(function(){mailWindow.close()}, 500);
             alert("Form Submitted Successfully...");
             div_hide();
         };
